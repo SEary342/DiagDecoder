7f6724a859558ae8ca89901869e3215024a219f8
Sam Eary <samwise342@gmail.com>
2025-04-05 19:14:45 -0400

    Initial app template

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), viteSingleFile()],
+  define: {
+    "process.env": {},
+    "import.meta.env.APP_VERSION": JSON.stringify(
+      process.env.npm_package_version
+    ),
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.js",
+    coverage: {
+      provider: "v8",
+      exclude: [
+        "src/tests/**",
+        "vite.config.ts",
+        "src/vite-env.d.ts",
+        "coverage/**",
+        "eslint.config.js",
+      ],
+    },
+  },
+  build: {
+    outDir: "docs",
+  },
+})
